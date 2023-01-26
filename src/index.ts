c4bb6f538839c03fbfa73ab155c17e3c92595bb9
Charalampos Fanoulis <yo@fanoulis.dev>
2023-01-26 18:29:32 +0200

    add bolt template code

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,57 @@
-console.log(`hello world!`);
+// gonna need to ESMify this - TODO @cfanoulis
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { App, LogLevel } from '@slack/bolt';
+
+const app = new App({
+	token: process.env.SLACK_BOT_TOKEN,
+	signingSecret: process.env.SLACK_SIGNING_SECRET,
+	logLevel: LogLevel.DEBUG
+});
+
+app.use(async ({ next }) => {
+	await next();
+});
+
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+	// Filter out message events with subtypes (see https://api.slack.com/events/message)
+	if (message.subtype === undefined || message.subtype === 'bot_message') {
+		// say() sends a message to the channel where the event was triggered
+		await say({
+			blocks: [
+				{
+					type: 'section',
+					text: {
+						type: 'mrkdwn',
+						text: `Hey there <@${message.user}>!`
+					},
+					accessory: {
+						type: 'button',
+						text: {
+							type: 'plain_text',
+							text: 'Click Me'
+						},
+						action_id: 'button_click'
+					}
+				}
+			],
+			text: `Hey there <@${message.user}>!`
+		});
+	}
+});
+
+app.action('button_click', async ({ body, ack, say }) => {
+	// Acknowledge the action
+	await ack();
+	await say(`<@${body.user.id}> clicked the button`);
+});
+
+(async () => {
+	// Start your app
+	await app.start(Number(process.env.PORT) || 3000);
+
+	console.log('⚡️ Bolt app is running!');
+})();
